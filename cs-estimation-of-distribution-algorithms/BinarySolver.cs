0365acafea16c67d262d2dc2a9c9f7e03f8d85b1
xschen <xs0040@gmail.com>
2017-10-30 08:19:50 +0800

    binary and continuous solution representation

---
+++ cs-estimation-of-distribution-algorithms/BinarySolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDA
+{
+    public abstract class BinarySolver : BaseSolver<int>
+    {
+        public BinarySolver()
+        {
+
+        }
+
+        public delegate double CostEvaluationMethod(int[] x, object constraints);
+
+
+    }
+}
